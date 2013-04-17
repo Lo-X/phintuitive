5ea802243e1d241e4bef370bec4522a716d396dc
Boutter Loic <loic.boutter@gmail.com>
2013-04-17 23:13:01 +0200

    Changed 404 layout to match default layout

--- app/layouts/404.php
+++ app/layouts/404.php
@@ -2,22 +2,23 @@
 <html>
 	<head>
 		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-		<title><?php echo isset($title_for_layout) ? $title_for_layout : 'PhIntuitive' ?></title>
+		<title><?php echo isset($title_for_layout) ? $title_for_layout : 'PhIntuitive - 404 Not Found' ?></title>
 		<link rel="stylesheet" href="http://localhost/themes/default/style.css">
 	</head>
 	
 	<body>
 		
-		<header>
-			<h3><a href="<?php echo HOST ?>">PhIntuitive</a></h3>
-			<ul class="nav">
-				<?php echo isset($menu_for_layout) ? $menu_for_layout : '' ?>
-			</ul>
+		<header class="header">
+			<h3><a href="<?php echo App::host() ?>">PhIntuitive</a></h3>
 		</header>
 		
 		<div class="container" style="padding-top: 60px;">
 			<h1>Erreur 404 - Page Non Trouvée</h1>
 			<?php echo $content_for_layout ?>
 		</div>
+
+		<footer class="footer">
+			<?php echo '<p>Page générée en '.round(microtime(true)-BEGIN_TIME, 5).'sec</p>' ?>
+		</footer>
 	</body>
 </html>
